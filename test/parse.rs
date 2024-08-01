4ec43170574f36ccd4977a4781f395fc02b8c868
Kentaro Tatsumi <paqira.2019@gmail.com>
2024-08-01 16:22:40 +0900

    add test

--- test/parse.rs
+++ test/parse.rs
@@ -28,3 +28,23 @@ fn empty_comment() {
     let isg = from_str(&s).unwrap();
     assert_eq!(s, isg.to_string())
 }
+
+#[test]
+fn minified() {
+    let s = fs::read_to_string("rsc/isg/example.1.minify.isg").unwrap();
+    let minified = from_str(&s).unwrap();
+
+    let s = fs::read_to_string("rsc/isg/example.1.isg").unwrap();
+    let expected = from_str(&s).unwrap();
+    assert_eq!(minified, expected)
+}
+
+#[test]
+fn many_space() {
+    let s = fs::read_to_string("rsc/isg/example.1.many_space.isg").unwrap();
+    let minified = from_str(&s).unwrap();
+
+    let s = fs::read_to_string("rsc/isg/example.1.isg").unwrap();
+    let expected = from_str(&s).unwrap();
+    assert_eq!(minified, expected)
+}
